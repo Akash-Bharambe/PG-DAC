4d09ce18a3d2cd099eef072af674e50f22a18091
Akash Bharambe <161416466+Akash-Bharambe@users.noreply.github.com>
2024-03-15 18:20:43 +0530

    Update 24_arrayMaxMinMul.cpp

--- C++ file/24_arrayMaxMinMul.cpp
+++ C++ file/24_arrayMaxMinMul.cpp
@@ -1,53 +1,43 @@
-#include<iostream>
+#include <iostream>
 #define SIZE 5
 using namespace std;
 
-void maximum(int maxi[]){
-            int val;
-        for(int i=0;i<SIZE;i++){
-        for(int j=0;j<SIZE;j++){
-            if(maxi[i]<maxi[j]){
-                val=maxi[j];
-            }
-
-        }
-    }
-    cout<<val<<endl;
-}
-void min(int mini[]){
-            int val;
-        for(int i=0;i<SIZE;i++){
-        for(int j=0;j<SIZE;j++){
-            if(mini[i]>mini[j]){
-                val=mini[j];
-            }
-
-        }
-    }
-    cout<<val<<endl;
-}
-
-void mul(int mult[]){
-    
-    for(int i=0;i<SIZE;i++){
-        mult[i]=5*mult[i];
-    }
+void maximum(int arr[])
+{
+    int val;
     for (int i = 0; i < SIZE; i++)
-    {
-        cout<<mult[i]<<" ";
-    }
-    
+        for (int j = 0; j < SIZE; j++)
+            if (arr[i] < arr[j])
+                val = arr[j];
+
+    cout << val << endl;
+}
+void minimum(int arr[])
+{
+    int val;
+    for (int i = 0; i < SIZE; i++)
+        for (int j = 0; j < SIZE; j++)
+            if (arr[i] > arr[j])
+                val = arr[j];
+    cout << val << endl;
 }
 
+void fiveTimes(int arr[])
+{
 
-int main(){
-    
+    for (int i = 0; i < SIZE; i++)
+        arr[i] = 5 * arr[i];
+    for (int i = 0; i < SIZE; i++)
+        cout << arr[i] << " ";
+}
+
+int main()
+{
+
     int arr[SIZE];
-    for(int i=0;i<SIZE;i++){
-        cin>>arr[i];
-    }
+    for (int i = 0; i < SIZE; i++)
+        cin >> arr[i];
     maximum(arr);
-    min(arr);
-    mul(arr);
-
+    minimum(arr);
+    fiveTimes(arr);
 }
