29782c205c62ce084b2bef1eb31601487694d667
Akash-Bharambe <aakashbharambe100@gmail.com>
2024-03-18 21:36:03 +0530

    a

---
+++ Cpp/SB Assignments/Q1_date.cpp
@@ -0,0 +1,16 @@
+// Q1. Write a menu driven program for Date in a Cpp. Declare a structure Date having data members day, month, year. Implement the following functions.
+// void initDate(struct Date* ptrDate);
+// void printDateOnConsole(struct Date* ptrDate);
+// void acceptDateFromConsole(struct Date* ptrDate);
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+
+
+int main()
+{
+    
+    return 0;
+}
