f430d826ad8f15a5bb2f07c8b68d0871a9ae3211
Akash-Bharambe <aakashbharambe100@gmail.com>
2024-04-03 21:29:29 +0530

    added

--- Java/Code/03-04-24/Point2D/src/com/cdac/tester/Tester.java
+++ Java/Code/03-04-24/Point2D/src/com/cdac/tester/Tester.java
@@ -1,30 +1,34 @@
 package com.cdac.tester;
+
 import com.cdac.core.Point2D;
 import java.util.Scanner;
-class Tester{
-	public static void main(String[] args){
+
+class Tester {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter x coordinates: ");
 		double x = sc.nextDouble();
 		System.out.println("Enter y coordinates: ");
 		double y = sc.nextDouble();
-		
-		Point2D p = new Point2D(x,y);
-		
-		System.out.println("Enter x coordinates: ");
+
+		Point2D p = new Point2D(x, y);
+
+		System.out.println("Enter x1 coordinates: ");
 		double x1 = sc.nextDouble();
-		System.out.println("Enter y coordinates: ");
+		System.out.println("Enter y1 coordinates: ");
 		double y1 = sc.nextDouble();
-		
-		Point2D p1 = new Point2D(x1,y1);
-		
+
+		Point2D p1 = new Point2D(x1, y1);
+
 		System.out.println(p.show());
 		System.out.println(p1.show());
-		
-		if(p.isEqual(p1)) System.out.println("Equal");
-		else System.out.println("Not Equal");
-		
-		System.out.println("Distance :"+ p.calculateDistance(p1));
+
+		if (p.isEqual(p1))
+			System.out.println("Equal");
+		else
+			System.out.println("Not Equal, Distance :" + p.calculateDistance(p1));
+
+		sc.close();
 	}
 }
