63bd69512b32ba4192ded19da4a9f684c5398de0
Akash Bharambe <161416466+Akash-Bharambe@users.noreply.github.com>
2024-04-04 16:43:17 +0530

    added

---
+++ Java/Code/04-04-24/src/com/developer/geometry/Point2D.java
@@ -0,0 +1,23 @@
+package com.developer.geometry;
+
+public class Point2D {
+	private double x;
+	private double y;
+
+	public Point2D(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public String show() {
+		return "x: " + x + " y: " + y + "\n";
+	}
+
+	public boolean isEqual(Point2D p) {
+		return ((this.x == p.x) && (this.y == p.y));
+	}
+
+	public double calculateDistance(Point2D p) {
+		return Math.sqrt((this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y));
+	}
+}
---
+++ Java/Code/04-04-24/src/com/tester/Tester.java
@@ -0,0 +1,39 @@
+package com.tester;
+
+import java.util.Scanner;
+
+import com.developer.geometry.Point2D;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("How many points to create: ");
+
+		Point2D[] points = new Point2D[sc.nextInt()];
+
+		for (int i = 0; i < points.length; i++) {
+
+			System.out.println("Enter x coordinates: ");
+			double x = sc.nextDouble();
+			System.out.println("Enter y coordinates: ");
+			double y = sc.nextDouble();
+			points[i] = new Point2D(x, y);
+
+			for (int j = 0; j <= i - 1; j++) {
+				if (points[i].isEqual(points[j])) {
+					points[i] = null;
+					System.out.println("These Coordinates already exists");
+					i--;
+					break;
+				}
+			}
+		}
+		for (Point2D p : points) {
+			System.out.println(p.show());
+		}
+		sc.close();
+	}
+
+}
