bf1637c9cdedf5ca43e8e691d9cbeb4c03541b20
Akash Bharambe <161416466+Akash-Bharambe@users.noreply.github.com>
2024-05-14 13:33:04 +0530

    added

--- WBT/JS/Assi26_1/script.js
+++ WBT/JS/Assi26_1/script.js
@@ -33,40 +33,86 @@ window.addEventListener("load", () => {
     year.innerHTML += `<option value="${i}">${i}</option>`;
 });
 
-document.getElementById("btn").addEventListener("click", () => {
-  document.getElementById("out").innerHTML = `You Entered: <br>
+document.getElementById("btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  if (validateData()) {
+    document.getElementById("out").innerHTML = `You Entered: <br>
     Name: ${name1.value}<br>
-    BirthDate: ${(bmonth.options[bmonth.selectedIndex].value).slice(0, 3)}/${bdate.options[bdate.selectedIndex].value}/${year.options[year.selectedIndex].value}<br>
+    BirthDate: ${bmonth.options[bmonth.selectedIndex].value.slice(0, 3)}/${
+      bdate.options[bdate.selectedIndex].value
+    }/${year.options[year.selectedIndex].value}<br>
     Email Address: ${email.value}<br>
     Gender: ${getData(gender)}<br>
     Lucky Number: ${luckyNo.value}<br>
     Favourite Food: ${getData(favFood)}`;
+  }
 });
 
-// document.getElementById("btn").addEventListener("click", () => {
-//     const selectedGender = document.querySelector('input[name="gender"]:checked');
-//     const selectedFavFoods = document.querySelectorAll('input[name="fav-food"]:checked');
-    
-//     document.getElementById("out").innerHTML = `You Entered: <br>
-//       Name: ${name1.value}<br>
-//       BirthDate: ${(bmonth.options[bmonth.selectedIndex].value).slice(0, 3)}/${bdate.options[bdate.selectedIndex].value}/${year.options[year.selectedIndex].value}<br>
-//       Email Address: ${email.value}<br>
-//       Gender: ${selectedGender ? selectedGender.value : ''}<br>
-//       Lucky Number: ${luckyNo.value}<br>
-//       Favourite Food: ${getData(selectedFavFoods)}`;
-//   });
-  
+function validateData() {
+  let flag = [true, true, true, true, true, true];
+  let err = document.getElementsByClassName('err')
+  for(let e of err){
+    e.style.color = 'red'
+    e.innerHTML = ''
+  }
+  if (name1.value === "") {
+    flag[0] = false;
+    document.getElementById(
+      "nameerr"
+    ).innerHTML = `Please Enter Name`;
+  }
+  if (
+    bmonth.selectedIndex === 0 ||
+    bdate.selectedIndex === 0 ||
+    year.selectedIndex === 0
+  ) {
+    flag[1] = false;
+    document.getElementById(
+      "bdateerr"
+    ).innerHTML = `Please Select Date`;
+  }
+  if ( getData(gender) === '') {
+    flag[2] = false;
+    document.getElementById(
+      "gendererr"
+    ).innerHTML = `Please Select Gender`;
+  }
+  if (email.value === "") {
+    flag[3] = false;
+    document.getElementById(
+      "mailerr"
+    ).innerHTML = `Please Enter Mail`;
+  }
+  if (luckyNo.value === "") {
+    flag[4] = false;
+    document.getElementById(
+      "luckyerr"
+    ).innerHTML = `Please Enter Lucky Number`;
+  }
+  if (getData(favFood) === '') {
+    flag[5] = false;
+    document.getElementById(
+      "faverr"
+    ).innerHTML = `Please Select Fav Food`;
+  }
 
-function getData(arr) {
-    let str = "";
-    for (let v of arr) {
-      if (v.type === 'radio' && v.checked) {
-        str = v.value;
-        return str
-      } else if (v.type === 'checkbox' && v.checked) {
-        str += v.value + ",";
-        return str.slice(0, str.length - 1);
-      }
+  for (let bool of flag) {
+    if (!bool) {
+      return false;
     }
   }
-  
+  return true;
+}
+
+function getData(arr) {
+  let str = "";
+  for (let v of arr) {
+    if (v.type === "radio" && v.checked) {
+      str = v.value;
+      return str;
+    } else if (v.type === "checkbox" && v.checked) {
+      str += v.value + ",";
+    }
+  }
+  return str.slice(0, str.length - 1);
+}
