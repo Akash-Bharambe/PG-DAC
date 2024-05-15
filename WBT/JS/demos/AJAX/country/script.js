840928171194c7b37d53e738e9c6645007a4551d
Akash Bharambe <161416466+Akash-Bharambe@users.noreply.github.com>
2024-05-15 10:44:37 +0530

    added

--- WBT/JS/demos/AJAX/country/script.js
+++ WBT/JS/demos/AJAX/country/script.js
@@ -1,14 +1,68 @@
 $(window).on("load", function () {
-    var headers = new Headers();
-headers.append("X-CSCAPI-KEY", "API_KEY");
   $.ajax({
-    url: "https://api.countrystatecity.in/v1/countries",
+    url: "https://restcountries.com/v3.1/all",
     type: "GET",
     async: true,
-    headers: headers,
-    redirect: 'follow',
-    success: function (result) {console.log(result);},
-    error: function (err) {console.log(err);},
+    success: function (result) {
+      addCountries(result);
+    },
+    error: function (err) {
+      console.log(err);
+    },
   });
 });
+var arr = [];
+function addCountries(result) {
+  result.sort((a, b) => {
+    return a.name.common > b.name.common
+      ? 1
+      : a.name.common < b.name.common
+      ? -1
+      : 0;
+  });
+  for (let country of result) {
+    $("#country").html(
+      $("#country").html() +
+        `<option value="${country.name.common}">${country.name.common}</option>`
+    );
+  }
+  arr = result;
+}
 
+$("#country").on("change", () => {
+  showFlag("#country", arr);
+});
+
+function showFlag(id, result) {
+  for (let country of result) {
+    if (country.name.common === $(id).val()) {
+      $("body").css({
+        "background-image": `url("${country.flags.png}")`,
+        "background-repeat": `no-repeat`,
+        "background-size": "cover",
+        "background-attachment": "fixed",
+        "background-position": "center center",
+      });
+      return;
+    }
+  }
+}
+
+
+// $.ajax({
+//   url: "./myfile.json",
+//   type: "GET",
+//   async : true,
+//   success: function (res) {
+//       console.log(res);
+//   },
+//   error:function (err) {
+//       console.log(err);
+//   }
+// })
+
+$.getJSON("myfile.json", function(data) {
+  console.log(data);
+}).fail(function() {
+  console.log("An error has occurred.");
+});
