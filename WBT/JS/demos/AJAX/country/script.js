c47037a38b4fef08a6c3cc57a74f7dbb514224eb
Akash-Bharambe <aakashbharambe100@gmail.com>
2024-05-15 23:16:25 +0530

    added

--- WBT/JS/demos/AJAX/country/script.js
+++ WBT/JS/demos/AJAX/country/script.js
@@ -1,68 +1,110 @@
-$(window).on("load", function () {
-  $.ajax({
-    url: "https://restcountries.com/v3.1/all",
-    type: "GET",
-    async: true,
-    success: function (result) {
-      addCountries(result);
-    },
-    error: function (err) {
-      console.log(err);
-    },
-  });
-});
-var arr = [];
-function addCountries(result) {
-  result.sort((a, b) => {
-    return a.name.common > b.name.common
-      ? 1
-      : a.name.common < b.name.common
-      ? -1
-      : 0;
-  });
-  for (let country of result) {
-    $("#country").html(
-      $("#country").html() +
-        `<option value="${country.name.common}">${country.name.common}</option>`
-    );
-  }
-  arr = result;
-}
+// $(window).on("load", function () {
+//   $.ajax({
+//     url: "https://restcountries.com/v3.1/all",
+//     type: "GET",
+//     async: true,
+//     success: function (result) {
+//       addCountries(result);
+//     },
+//     error: function (err) {
+//       console.log(err);
+//     },
+//   });
+// });
+// $.getJSON("countries+states+cities.json", function (data) {
+//   addCountries(data);
+// }).fail(function () {
+//   console.log("An error has occurred.");
+// });
 
-$("#country").on("change", () => {
-  showFlag("#country", arr);
+var countriesData = [];
+var selectedCountry = "";
+var selectedState = "";
+var selectedCity = "";
+
+$.ajax({
+  url: "./countries+states+cities.json",
+  type: "GET",
+  async: true,
+  success: function (res) {
+    countriesData = res;
+    addCountries(res);
+  },
+  error: function (err) {
+    console.log(err);
+  },
 });
 
-function showFlag(id, result) {
-  for (let country of result) {
-    if (country.name.common === $(id).val()) {
-      $("body").css({
-        "background-image": `url("${country.flags.png}")`,
-        "background-repeat": `no-repeat`,
-        "background-size": "cover",
-        "background-attachment": "fixed",
-        "background-position": "center center",
-      });
+$("#country").on("change", function () {
+  selectedCountry = $(this).val();
+
+  for (let country of countriesData) {
+    if (country.name === selectedCountry) {
+      addStates(country.states);
       return;
     }
   }
+});
+
+$("#state").on("change", function () {
+  selectedState = $(this).val();
+  for (let country of countriesData) {
+    if (country.name === selectedCountry) {
+      for (let state of country.states) {
+        if (state.name === selectedState) {
+          addCities(state.cities);
+          return;
+        }
+      }
+    }
+  }
+});
+
+$("#city").on("change", function () {
+  selectedCity = $(this).val();
+  console.log(`Country: ${selectedCountry}
+State: ${selectedState}
+City: ${selectedCity}`);
+});
+
+function addCountries(countries) {
+  countries.sort((a, b) => {
+    return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
+  });
+
+  for (let country of countries) {
+    $("#country").html(
+      $("#country").html() +
+        `<option value="${country.name}">${country.name}</option>`
+    );
+  }
 }
 
+function addStates(states) {
+  console.log(states);
+  $("#state").html('<option value="select">Select State</option>');
+  states.sort((a, b) => {
+    return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
+  });
 
-// $.ajax({
-//   url: "./myfile.json",
-//   type: "GET",
-//   async : true,
-//   success: function (res) {
-//       console.log(res);
-//   },
-//   error:function (err) {
-//       console.log(err);
-//   }
-// })
+  for (let state of states) {
+    $("#state").html(
+      $("#state").html() +
+        `<option value="${state.name}">${state.name}</option>`
+    );
+  }
+}
 
-$.getJSON("myfile.json", function(data) {
-  console.log(data);
-}).fail(function() {
-  console.log("An error has occurred.");
-});
+function addCities(cities) {
+  console.log(cities);
+  $("#city").html('<option value="select">Select City</option>');
+  cities.sort((a, b) => {
+    return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
+  });
+
+  for (let city of cities) {
+    $("#city").html(
+      $("#city").html() + `<option value="${city.name}">${city.name}</option>`
+    );
+  }
+}
