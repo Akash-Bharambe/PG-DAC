02a1cfa001facc6626ad854fcac44452bec2da61
Akash-Bharambe <aakashbharambe100@gmail.com>
2024-06-15 00:01:51 +0530

    added

--- Adv JAVA/Products/src/main/java/com/products/service/ProductsService.java
+++ Adv JAVA/Products/src/main/java/com/products/service/ProductsService.java
@@ -9,11 +9,7 @@ public interface ProductsService {
 	
 	List<Product> getAllProducts();
 	Product addProduct(Product product);
-<<<<<<< HEAD
 	String updateProduct(Long id, Product product);
 	String deleteProduct(Long id);
 	Product getproduct(Long id);
-=======
-	Product updateProduct(Product product);
->>>>>>> 5922645 (added)
 }
