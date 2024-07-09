45507583499151490a08e42e6cb14a9c3f926460
Akash Bharambe <161416466+Akash-Bharambe@users.noreply.github.com>
2024-07-09 10:20:10 +0530

    done

---
+++ dotnet/Employee/Arrays/Program.cs
@@ -0,0 +1,66 @@
+﻿using Employee_Dictionary;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Number of Employees: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Employee[] employees = new Employee[num];
+
+            // Accept details for all Employees
+            for (int i = 0; i < employees.Length; i++ )
+            {
+                Console.Write("Enter Employee Id: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Employee Name: ");
+                string? name = Console.ReadLine();
+
+                Console.Write("Enter Basic Salary : ");
+                decimal basic = Convert.ToDecimal(Console.ReadLine());
+
+                Console.Write("Enter Department Number : ");
+                short deptno = Convert.ToInt16(Console.ReadLine());
+
+                employees[i] = new Employee(id, name, basic, deptno);
+                Console.WriteLine();
+            }
+
+            // Display the Employee with highest Salary
+
+            decimal maxSal = decimal.MinValue;
+            int index = -1;
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i].BasicSalary > maxSal)
+                {
+                    maxSal = employees[i].BasicSalary;
+                    index = i;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Employee with highest Salary: ");
+            Console.WriteLine(employees[index]);
+
+
+            // Accept EmpNo to be searched.Display all details for that employee.
+            Console.WriteLine();
+            Console.Write("Enter Employee Number to search: ");
+            int empNo = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i].Id == empNo)
+                {
+                    Console.WriteLine(employees[i]);
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ dotnet/Employee/Employee_Dictionary/Utils.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Dictionary
+{
+    internal class Utils
+    {
+
+        internal static int MenuList()
+        {
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Accept Employee Details");
+            Console.WriteLine("2. Employee with highest salary");
+            Console.WriteLine("3. Search Employee");
+            Console.WriteLine("4. Nth Employee");
+            Console.WriteLine("5. Display All Employees");
+            Console.Write("Enter Choice: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        internal static void AcceptEmployees(Dictionary<int, Employee> employees)
+        {
+            char ch;
+            do
+            {
+                Console.Write("Enter Employee Id: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Employee Name: ");
+                string? name = Console.ReadLine();
+
+                Console.Write("Enter Basic Salary : ");
+                decimal basic = Convert.ToDecimal(Console.ReadLine());
+
+                Console.Write("Enter Department Number : ");
+                short deptno = Convert.ToInt16(Console.ReadLine());
+
+                employees.Add(id, new Employee(id, name, basic, deptno));
+
+                Console.Write("Do You Want to Continue(y/n)? ");
+                ch = Convert.ToChar(Console.Read());
+                Console.ReadLine();
+            } while (ch != 'n');
+
+        }
+
+        internal static void DisplayAllEmployees(Dictionary<int, Employee> employees)
+        {
+            foreach (KeyValuePair<int, Employee> employee in employees)
+            {
+                Console.WriteLine(employee.Value);
+            }
+        }
+
+        internal static Employee GetEmployeeWithHighestSalary(Dictionary<int, Employee> employees)
+        {
+            int id = -1;
+            decimal maxSal = decimal.MinValue;
+
+            foreach (KeyValuePair<int, Employee> employee in employees)
+            {
+                if (employee.Value.BasicSalary > maxSal)
+                {
+                    maxSal = employee.Value.BasicSalary;
+                    id = employee.Key;
+                }
+            }
+
+            return employees[id];
+        }
+
+        internal static Employee GetNthEmployee(Dictionary<int, Employee> employees, int index)
+        {
+            return employees.ElementAtOrDefault(index).Value;
+        }
+
+        internal static Employee SearchEmployee(Dictionary<int, Employee> employees, int id)
+        {
+            return employees[id];
+        }
+    }
+}
