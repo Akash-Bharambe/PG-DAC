027e9f4a80e17c37c4b82594b0eb110e1c9ce343
Akash Bharambe <161416466+Akash-Bharambe@users.noreply.github.com>
2024-07-13 13:08:22 +0530

    done

---
+++ dotnet/Assignments/FileHandling/Employee.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandling
+{
+    [DataContract]
+    public class Employee
+    {
+        private string? name;
+        
+        [DataMember]
+        public string? Name
+        {
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    name = value;
+                else
+                    throw new ArgumentException("Name cannot be Blank");
+            }
+
+            get => name;
+        }
+
+        private int id;
+        
+        [DataMember]
+        public int Id
+        {
+            set
+            {
+                if (int.IsPositive(value)) id = value;
+                else throw new ArgumentException("Id cannot be negative");
+            }
+            get => id;
+        }
+
+        private decimal basicSalary;
+
+        [DataMember]
+        public decimal BasicSalary
+        {
+            set
+            {
+                if (value >= 50000 && value <= 100000) basicSalary = value;
+                else throw new ArgumentException("Basic Salary must be between 50000-100000");
+            }
+            get => basicSalary;
+        }
+
+        private short deptNo;
+
+        [DataMember]
+        public short DeptNo
+        {
+            set
+            {
+                if (int.IsPositive(value)) deptNo = value;
+                else throw new ArgumentException("Department Number cannot be negative");
+            }
+            get => deptNo;
+
+        }
+
+        public Employee()
+        {
+            
+        }
+
+        public Employee(int id, string? name = "Default", decimal basicSalary = 50000, short deptNo = 100)
+        {
+            Id = id;
+            Name = name;
+            BasicSalary = basicSalary;
+            DeptNo = deptNo;
+        }
+
+
+        public decimal GetNetSalary()
+        {
+            return basicSalary * (1 + 0.25m);
+        }
+
+        public override string ToString()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            return $"{id} . {name} | salary = {GetNetSalary().ToString("C", CultureInfo.CreateSpecificCulture("hi-IN"))} | Department No =  {deptNo}";
+        }
+
+    }
+}
---
+++ dotnet/Assignments/FileHandling/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Linq;
+
+namespace FileHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees =
+            [
+                new Employee(1, "John Doe", 75000m, 101),
+                new Employee(2, "Jane Smith", 80000m, 102),
+                new Employee(3, "Michael Johnson", 90000m, 103)
+            ];
+            Employee emp1 = new(1, "John Doe", 98765m);
+            //WriteObject(emp1);
+            //Console.WriteLine(ReadObject(emp1));
+            WriteCollection(employees);
+            IEnumerable? readEmployees = ReadCollections<Employee>().Where(emp => emp != null);
+            foreach (var emp in readEmployees)
+            {
+                Console.WriteLine(emp.ToString());
+            }
+        }
+
+        public static void WriteObject(object obj)
+        {
+            DataContractJsonSerializer json = new(obj.GetType());
+            Stream stream = new FileStream("D:\\Akash\\dotnet\\Assignments\\FileHandling\\Employee.json", FileMode.Create);
+            json.WriteObject(stream, obj);
+            Console.WriteLine("Written Successfully!");
+            stream.Close();
+        }
+
+        public static T? ReadObject<T>()
+        {
+            DataContractJsonSerializer json = new(typeof(T));
+            Stream input = new FileStream("D:\\Akash\\dotnet\\Assignments\\FileHandling\\Employee.json", FileMode.Open);
+
+            T? emp = (T?)json.ReadObject(input);
+            input.Close();
+            return emp;
+        }
+
+        public static void WriteCollection(IEnumerable obj)
+        {
+            DataContractJsonSerializer json = new(obj.GetType());
+            using (Stream stream = new FileStream("D:\\Akash\\dotnet\\Assignments\\FileHandling\\Employee.json", FileMode.Create))
+            {
+                json.WriteObject(stream, obj);
+            }
+            Console.WriteLine("Written Successfully!");
+        }
+
+        public static IEnumerable<T>? ReadCollections<T>()
+        {
+            DataContractJsonSerializer json = new(typeof(List<T>));
+            using (Stream input = new FileStream("D:\\Akash\\dotnet\\Assignments\\FileHandling\\Employee.json", FileMode.Open))
+            {
+                List<T>? list = (List<T>?)json.ReadObject(input);
+                return list;
+            }
+        }
+
+        //public static void WriteCollection(IEnumerable obj)
+        //{
+        //    DataContractJsonSerializer json = new DataContractJsonSerializer(obj.GetType());
+        //    using (Stream stream = new FileStream("D:\\Akash\\dotnet\\Assignments\\FileHandling\\Employee.json", FileMode.Create))
+        //    {
+        //        json.WriteObject(stream, obj);
+        //    }
+        //    Console.WriteLine("Written Successfully!");
+        //}
+
+        //public static IEnumerable<T>? ReadCollections<T>()
+        //{
+        //    DataContractJsonSerializer json = new(typeof(T));
+        //    Stream input = new FileStream("D:\\Akash\\dotnet\\Assignments\\FileHandling\\Employee.json", FileMode.Open);
+        //    T? obj;
+        //    while ((obj = (T?)json.ReadObject(input)) != null)
+        //    {
+        //        yield return obj;
+        //    }
+        //    input.Close();
+        //}
+
+    }
+}
